fc02bcc38db69e157f649b2ee633671338071de0
Landon Grinderslev <125698291+lgrinders@users.noreply.github.com>
2024-10-10 15:41:21 -0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/RecipApp-Sunday-Salad/",
+  base: "/RecipeApp-Sunday-Salad/",
   build: {
     sourcemap: false,
   },
