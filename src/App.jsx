edaf9049eaf87c7867144d5e61e4d9e8a6414220
Landon Grinderslev <125698291+lgrinders@users.noreply.github.com>
2024-10-14 12:56:52 -0500

    fix: browser router base

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,18 @@ import NavBar from "./components/NavBar/index";
 import Details from "./pages/Details/index";
 import Favorites from "./pages/Favorites/index";
 import Home from "./pages/Home/index";
-import { Routes, Route, Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <Router basename="/RecipeApp-Sunday-Salad">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/recipe-item/:id" element={<Details />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/recipe-item/:id" element={<Details />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
