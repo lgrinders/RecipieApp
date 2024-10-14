46b838b04632ee1dd0c6ed520f7a65699d26615d
Landon Grinderslev <125698291+lgrinders@users.noreply.github.com>
2024-10-14 12:27:42 -0500

    fix: added homepage property and route basename

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import NavBar from "./components/NavBar/index";
 import Details from "./pages/Details/index";
 import Favorites from "./pages/Favorites/index";
 import Home from "./pages/Home/index";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path="/lgrinders.github.io/RecipeApp-Sunday-Salad/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/recipe-item/:id" element={<Details />} />
-      </Routes>
-      <Footer />
+      <Router basename="/RecipeApp-Sunday-Salad">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/recipe-item/:id" element={<Details />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
