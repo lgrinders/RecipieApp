011c47b8c1be07102d77ba186b2a4c1c6003b8ab
Landon Grinderslev <125698291+lgrinders@users.noreply.github.com>
2024-10-14 12:18:40 -0500

    update: home url

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
     <>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/https://lgrinders.github.io/RecipeApp-Sunday-Salad/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/recipe-item/:id" element={<Details />} />
       </Routes>
